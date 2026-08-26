32773a1256f5cc73664836d829ec6b44fc5e8818
Michael Shilman <shilman@users.noreply.github.com>
2026-08-26 10:38:08 +0800

    Update documentation heading text

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
-          <h2>Documentation</h2>
+          <h2>Documentation BBB</h2>
           <p>Your questions, answered</p>
           <ul>
             <li>
