3979147cbf9d4f9f2c1f9401bf2055a7f6a3a59d
Michael Shilman <shilman@users.noreply.github.com>
2026-08-26 10:41:19 +0800

    Update community message in App component

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
             <use href="/icons.svg#social-icon"></use>
           </svg>
           <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
+          <p>Join the Vite community BBB</p>
           <ul>
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
